77ad78b5a9cab0b37d53523e60253715014fb265
iterhim <78038892+iterhim@users.noreply.github.com>
2022-03-09 21:08:15 +0200

    Add files via upload

---
+++ 4 task/app.js
@@ -0,0 +1,17 @@
+// let name = prompt();
+// let lname = prompt();
+
+// let createNewUser = function (a, b) {
+let createNewUser = function () {
+    let newUser = {
+        firstName: prompt(),
+        lastName: prompt(),
+        getLogin: function () {
+            let get = this.firstName[0] + this.lastName;
+            console.log(get.toLowerCase());
+        }
+    }
+    newUser.getLogin()
+}
+createNewUser();
+// createNewUser(name, lname);
