9d873012b3c27b763cbb6ac05366b565893eac18
iterhim <78038892+iterhim@users.noreply.github.com>
2022-03-11 21:56:57 +0200

    Add files via upload

--- 2 task/app.js
+++ 2 task/app.js
@@ -1,3 +1,5 @@
+// @iterhim
+
 // let number = prompt("Введіть число");
 let number = prompt();
 let arr = [];
@@ -12,7 +14,19 @@ if (arr.length <= 1) {
     alert(arr);
 
 }
+// Не плутай alert та console.log! В завданні сказано вивести в консоль а не модальше вікно
 
+// @vitaliykyq
 
+let number = prompt();
+let check = false;
 
+// З 1 а не з 0, тому що 0 і так завжди ділиться на ціло на 5
+for (let i = 1; i <= number; i++) {
+    if(i % 5 === 0){
+        check = true;
+        console.log(i);
+    }
+}
 
+if(!check) console.log("Sorry, no numbers");
