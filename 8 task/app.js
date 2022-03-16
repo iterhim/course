512f39e7b281eb1dde255937dc1df69f7c9204bc
iterhim <78038892+iterhim@users.noreply.github.com>
2022-03-16 17:18:33 +0200

    Add files via upload

--- 8 task/app.js
+++ 8 task/app.js
@@ -1,25 +1,20 @@
-let func = function (){
-
-    let first = document.getElementById('lists');
-    let second = document.getElementById('asd');
-    if(onmouseout === null){
-        if(second.value < 0){
-            first.innerHTML = "<h6 class='last'>'Please enter correct price '</h6> ";
-            document.querySelector('#asd').classList.add('myClass');
-
-            second.value = "";
-
-        }else{
-            document.querySelector('#asd').classList.remove('myClass');
-            first.innerHTML = `<div class="out">Текущая цена: ${second.value} <button id="butt">||X||</button></div> `;
-            // second.style.color = "green"
-            second.value = "";
-        }
+let first = document.getElementById('lists');
+let second = document.getElementById('asd');
+if (onmouseout === null) {
+    if (second.value <= 0) {
+        first.innerHTML = "<h6 class='last'>'Please enter correct price '</h6> ";
+        document.querySelector('#asd').classList.add('myClass');
+        second.value = "";
+    } else {
+        document.querySelector('#asd').classList.remove('myClass');
+        first.innerHTML = `<div class="out">Текущая цена: ${second.value} <button id="butt">||X||</button></div> `;
+        second.value = "";
     }
-    let third = document.getElementById('butt');
-    third.addEventListener('click', function() {
-        first.innerHTML = ""
-    });
-
-
 }
+let third = document.getElementById('butt');
+third.addEventListener('click', function () {
+    first.innerHTML = ""
+});
+
+
+
