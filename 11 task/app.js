c391180b7d396120c6ba33f4118886b2db033e2c
iterhim <78038892+iterhim@users.noreply.github.com>
2022-03-16 16:36:07 +0200

    Add files via upload

--- 11 task/app.js
+++ 11 task/app.js
@@ -6,69 +6,52 @@ let arbtn = document.getElementsByClassName('btn');
 document.addEventListener('keypress', (event) => {
     let keyName = event.key;
     console.log(keyName)
+    let del = function () {
+        arbtn[0].classList.remove("asr")
+        arbtn[1].classList.remove("asr")
+        arbtn[2].classList.remove("asr")
+        arbtn[3].classList.remove("asr")
+        arbtn[4].classList.remove("asr")
+        arbtn[5].classList.remove("asr")
+        arbtn[6].classList.remove("asr")
+    };
     for (let i = 0; i < arbtn.length; i++) {
         let a = arbtn[i].outerText
-        if (keyName === arbtn[i].value) {
-            console.log(i);
-            if (i === 0){
-                arbtn[i].classList.add("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
 
-            }else if (i === 1){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
-            }else if (i === 2){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
-            }else if (i === 3){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
-            }else if (i === 4){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
-            }else if (i === 5){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[6].classList.remove("asr")
-            }else if (i === 6){
-                arbtn[i].classList.add("asr")
-                arbtn[0].classList.remove("asr")
-                arbtn[1].classList.remove("asr")
-                arbtn[2].classList.remove("asr")
-                arbtn[3].classList.remove("asr")
-                arbtn[4].classList.remove("asr")
-                arbtn[5].classList.remove("asr")
+        if (keyName === arbtn[i].value) {
+            switch (i) {
+                case (0):
+                    del();
+                    arbtn[0].classList.add("asr");
+                    break;
+                case 1:
+                    del();
+                    arbtn[1].classList.add("asr");
+                    break;
+                case 2:
+                    del();
+                    arbtn[2].classList.add("asr");
+                    break;
+                case 3:
+                    del();
+                    arbtn[3].classList.add("asr");
+                    break;
+                case 4:
+                    del();
+                    arbtn[4].classList.add("asr");
+                    break;
+                case 5:
+                    del();
+                    arbtn[5].classList.add("asr");
+                    break;
+                case 6:
+                    del();
+                    arbtn[6].classList.add("asr");
+                    break;
             }
         }
+
+
     }
 });
 
