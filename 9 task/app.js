9b73558463a63dcbe61811483e69262e66a7e07d
iterhim <78038892+iterhim@users.noreply.github.com>
2022-03-16 20:00:31 +0200

    Add files via upload

--- 9 task/app.js
+++ 9 task/app.js
@@ -1,57 +1,21 @@
 let app = function () {
-    let firstBtn = document.getElementById('firstBtn');
-    let secondBtn = document.getElementById('secondBtn');
-    let thirdBtn = document.getElementById('thirdBtn');
-    let fourBtn = document.getElementById('fourBtn');
-    let fiveBtn = document.getElementById('fiveBtn');
-    let Akali = document.getElementById('Akali');
-    let Anivia = document.getElementById('Anivia');
-    let Draven = document.getElementById('Draven');
-    let Garen = document.getElementById('Garen');
-    let Katarina = document.getElementById('Katarina');
-    let tabsContent = document.getElementById('tabsContent');
-    firstBtn.addEventListener('click', function () {
-        tabsContent.innerHTML = Akali.outerHTML;
-        firstBtn.classList.add("active");
-        secondBtn.classList.remove("active");
-        thirdBtn.classList.remove("active");
-        fourBtn.classList.remove("active");
-        fiveBtn.classList.remove("active");
+    let tabs = document.querySelector("#tabsContent")
+    let btn = document.querySelectorAll('.tabs-title');
+    let text = document.querySelectorAll('.text');
 
-    });
-    secondBtn.addEventListener('click', function () {
-        tabsContent.innerHTML = Anivia.outerHTML;
-        firstBtn.classList.remove("active");
-        secondBtn.classList.add("active");
-        thirdBtn.classList.remove("active");
-        fourBtn.classList.remove("active");
-        fiveBtn.classList.remove("active");
-    });
-    thirdBtn.addEventListener('click', function () {
-        tabsContent.innerHTML = Draven.outerHTML;
-        firstBtn.classList.remove("active");
-        secondBtn.classList.remove("active");
-        thirdBtn.classList.add("active");
-        fourBtn.classList.remove("active");
-        fiveBtn.classList.remove("active");
-    });
-    fourBtn.addEventListener('click', function () {
-        tabsContent.innerHTML = Garen.outerHTML;
-        firstBtn.classList.remove("active");
-        secondBtn.classList.remove("active");
-        thirdBtn.classList.remove("active");
-        fourBtn.classList.add("active");
-        fiveBtn.classList.remove("active");
-    });
-    fiveBtn.addEventListener('click', function () {
-        tabsContent.innerHTML = Katarina.outerHTML;
-        firstBtn.classList.remove("active");
-        secondBtn.classList.remove("active");
-        thirdBtn.classList.remove("active");
-        fourBtn.classList.remove("active");
-        fiveBtn.classList.add("active");
-    });
+    function add0() {
+        for (let i = 0; i < btn.length; i++) {
 
+            btn[i].classList.remove("active");
+        }
+    }
+    for (let j = 0; j < btn.length; j++) {
+        btn[j].addEventListener('click', function () {
+            tabs.innerHTML = text[j].outerHTML;
+            add0()
+            btn[j].classList.add("active");
+        });
+    }
 
 }
 app()
